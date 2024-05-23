a73759c6d1cb15b75e9e191566cc337ccdbe7b97
darwin.apolinario <darwin.apolinario@gmail.com>
2024-05-23 15:59:00 +0800

    :sparkles: feature: added data

---
+++ lib/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  light: {
+    black: '#0F172A',
+    aqua: '#5CE5D4',
+    white: '#E2E8F0',
+  },
+  dark: {
+    black: '#0F172A',
+    aqua: '#5CE5D4',
+    white: '#E2E8F0',
+  },
+};
---
+++ lib/data.ts
@@ -0,0 +1,84 @@
+export const data = {
+  user: {
+    fName: 'darwin',
+    lName: 'apolinario',
+    title: 'application developer',
+  },
+  metaData: {
+    page: {
+      title: 'Darwin Apolinario',
+      description: 'SWE',
+    },
+    navigation: [
+      {
+        name: 'about',
+        url: '#about',
+      },
+      {
+        name: 'experience',
+        url: '#experience',
+      },
+      {
+        name: 'projects',
+        url: '#projects',
+      },
+    ],
+  },
+  jobs: {
+    companies: [
+      {
+        title: 'Fe',
+        name: 'test',
+        tags: ['js', 'aws', 'go'],
+        description: 'test',
+        start: '2020',
+        end: '2100',
+      },
+      {
+        title: 'Fe',
+        name: 'test',
+        tags: ['js', 'aws', 'go'],
+        description: 'test',
+        start: '2020',
+        end: '2100',
+      },
+      {
+        title: 'Fe',
+        name: 'test',
+        tags: ['js', 'aws', 'go'],
+        description: 'test',
+        start: '2020',
+        end: '2100',
+      },
+    ],
+  },
+  projects: [
+    {
+      name: 'test proj',
+      description: 'test description',
+      image: '',
+      url: '',
+    },
+    {
+      name: 'test proj',
+      description: 'test description',
+      image: '',
+      url: '',
+    },
+    {
+      name: 'test proj',
+      description: 'test description',
+      image: '',
+      url: '',
+    },
+  ],
+  education: [
+    {
+      name: 'test ',
+      title: 'test',
+      start: 'test description',
+      end: 'test',
+      url: 'test',
+    },
+  ],
+};
